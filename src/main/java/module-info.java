a32daa8a0920ce6227c84174b0b2a61c79955ece
Sujeet <sujeet3212000@gmail.com>
2025-04-06 23:16:39 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.imagelist.imagelist {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.imagelist.imagelist to javafx.fxml;
+    exports org.imagelist.imagelist;
+}
